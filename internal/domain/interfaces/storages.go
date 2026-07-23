16de9fe0bba355f28dadf4677808e56da5001ec6
KedroPedro <zyazyulyam727@gmail.com>
2026-07-23 11:11:49 +0300

    Add interface for user repository

--- internal/domain/interfaces/storages.go
+++ internal/domain/interfaces/storages.go
@@ -6,6 +6,12 @@ import (
 	"github.com/KedroPedro/order-matching-engine/internal/domain/entity"
 )
 
+type UserRepository interface {
+	ReserveBalance(ctx context.Context, userId string, toReserve int64) error
+	ReleaseBalance(ctx context.Context, userId string, toReserve int64) error
+	UpdateBalance(ctx context.Context, userId string, reserved int64, spent int64) error
+}
+
 type OrderRepository interface {
 	AddToQueue(ctx context.Context, order *entity.Order) error
 	ProcessEvent(ctx context.Context, event *entity.Event) error
