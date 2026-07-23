ab64d697bc343cb34f8b0f37fa59e5bfa6db680a
KedroPedro <zyazyulyam727@gmail.com>
2026-07-23 17:48:38 +0300

    rename method in interface

--- internal/domain/interfaces/storages.go
+++ internal/domain/interfaces/storages.go
@@ -6,6 +6,12 @@ import (
 	"github.com/KedroPedro/order-matching-engine/internal/domain/entity"
 )
 
+type UserRepository interface {
+	ReserveBalance(ctx context.Context, userId string, toReserve int64) error
+	ReleaseBalance(ctx context.Context, userId string, toReserve int64) error
+	UpdateBalance(ctx context.Context, userId string, reserved int64, spent int64) error
+}
+
 type OrderRepository interface {
 	AddToQueue(ctx context.Context, order *entity.Order) error
 	ProcessEvent(ctx context.Context, event *entity.Event) error
@@ -14,7 +20,7 @@ type OrderRepository interface {
 type MarketStateRepository interface {
 	AddToQueue(ctx context.Context, order *entity.Order) error
 	ProcessEvent(ctx context.Context, event *entity.Event) error
-	GetLastPrice(ctx context.Context, orderType entity.OrderType) (int64, error)
+	GetBestPrice(ctx context.Context, orderType entity.OrderType) (int64, error)
 	GetState(ctx context.Context)
 }
 
