2b60d30222a9e8d36464d1dcb2b81824567f9d0b
KedroPedro <zyazyulyam727@gmail.com>
2026-08-09 20:00:00 +0300

    move BookStorage interface into book file

--- internal/infrastructure/engine/storages/orderbook/book.go
+++ internal/infrastructure/engine/storages/orderbook/book.go
@@ -5,6 +5,13 @@ import (
 	enginetypes "github.com/KedroPedro/order-matching-engine/internal/infrastructure/engine/engine_types"
 )
 
+type BookStorage interface {
+	Add(order *enginetypes.EngineOrder)
+	GetRange(quantity int64, price int64) []enginetypes.PriceLevel
+	GetFirst() *enginetypes.PriceLevel
+	Delete(level int64)
+}
+
 type Book struct {
 	ask       BookStorage
 	bid       BookStorage
